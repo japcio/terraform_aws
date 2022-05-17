110a237e87bfa3cc30b3dcb52076997b06490967
japcio <japcio@gmail.com>
2022-05-17 19:26:11 +0200

    create webserver

--- main.tf
+++ main.tf
@@ -5,12 +5,37 @@ provider "aws" {
 }
 
 
+resource "aws_security_group" "instance" {
+    name = "terraform-security-group"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+}
 resource "aws_instance" "example" {
     ami = "ami-00c90dbdc12232b58"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
     tags = {
-        Name = "terraform_example"
+        Name = "terraform_web_server"
     }
 
 }
+
