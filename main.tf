87b092e412ca3defb57a5f4641aa081951f6d35f
japcio <japcio@gmail.com>
2022-05-17 09:45:49 +0200

    commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+#main file for terraform
+
+provider "aws" {
+    region="eu-west-1"
+}
+
+
+resource "aws_instance" "example" {
+    ami = "ami-00c90dbdc12232b58"
+    instance_type = "t2.micro"
+}
